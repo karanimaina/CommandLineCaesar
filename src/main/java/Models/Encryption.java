06f8eca39b52aa247ebdf38edd3e99a361d4e770
felixmaina <karani.maina2010@gmail.com>
2022-01-20 22:01:54 +0300

    add a constructor

--- src/main/java/Models/Encryption.java
+++ src/main/java/Models/Encryption.java
@@ -2,4 +2,6 @@ package Models;
 
 public class Encryption {
 
+    public Encryption(String moringa, int i) {
+    }
 }
--- src/test/java/Models/EncryptionTest.java
+++ src/test/java/Models/EncryptionTest.java
@@ -1,12 +1,19 @@
 package Models;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncryptionTest {
-
+    Encryption encryption;
     @BeforeEach
     void setUp() {
+        encryption = new Encryption("MORINGA",2);
     }
+    @Test
+    public void Encryption_instanceOf_true() {
+        assertEquals(true, encryption instanceof Encryption);
+    }
+
 }
