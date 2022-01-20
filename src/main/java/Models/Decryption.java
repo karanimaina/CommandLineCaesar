4ed8ef26882a222fe27270d17784f3a593b2ee87
felixmaina <karani.maina2010@gmail.com>
2022-01-20 22:46:14 +0300

    add a decrypt method that decrypts encrypted message

--- src/main/java/Models/Decryption.java
+++ src/main/java/Models/Decryption.java
@@ -1,7 +1,43 @@
 package Models;
 
 public class Decryption {
+    private String decryptMessage;
+    private int shiftKey;
+    private String StringMessage;
 
-    public Decryption(String decryptMessage, int i) {
+    public static final String ALPHABETS = "abcdefghijklmnopqrstuvwxyz";
+
+    public Decryption(String decryptText, int shiftKey) {
+        this.decryptMessage = decryptText;
+        this.shiftKey = shiftKey;
+        this.StringMessage = "";
     }
+
+    public String getDecryptMessage() {
+        return decryptMessage;
+    }
+
+    public int getShiftKey() {
+        return shiftKey;
+    }
+
+    public String getStringMessage() {
+        return StringMessage;
+    }
+    public String decrypt(){
+        decryptMessage =decryptMessage.toLowerCase();
+        StringMessage = "";
+        for (int i = 0; i < decryptMessage.length(); i++) {
+            int charPosition = ALPHABETS.indexOf(decryptMessage.charAt(i));
+            int keyVal = ( charPosition- shiftKey) % 26;
+            if (keyVal < 0) {
+                keyVal = ALPHABETS.length() + keyVal;
+            }
+            char replaceVal = ALPHABETS.charAt(keyVal);
+            StringMessage += replaceVal;
+        }
+        return  StringMessage;
+
+    }
+}
 }
