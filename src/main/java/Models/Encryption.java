f23530624136831387bb5fcb0de50d6d8b4f7df2
felixmaina <karani.maina2010@gmail.com>
2022-01-20 22:20:46 +0300

    add an encrypt method that converts a string to an encrypted text

--- src/main/java/Models/Encryption.java
+++ src/main/java/Models/Encryption.java
@@ -1,7 +1,38 @@
 package Models;
 
-public class Encryption {
 
-    public Encryption(String moringa, int i) {
+public class Encryption {
+    private String message;
+    private int shiftKey;
+    private String cipherText;
+
+    public static final String alphabets = "abcdefghijklmnopqrstuvwxyz";
+
+
+    public Encryption(String message, int shiftKey) {
+        this.message = message;
+        this.shiftKey = shiftKey;
+        this.cipherText = "";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getShiftKey() {
+        return shiftKey;
+    }
+    public String encrypt(){
+        message= message.toLowerCase();
+        cipherText = "";
+        for (int i = 0; i < message.length(); i++) {
+            int charPosition = alphabets.indexOf(message.charAt(i));
+            int keyVal = (shiftKey + charPosition) % 26;
+            char replaceVal = alphabets.charAt(keyVal);
+            cipherText += replaceVal;
+        }
+        return  cipherText;
+
     }
 }
+
