bfba08bb6afdbfcc26ae2a1fb9776438dffab767
felixmaina <karani.maina2010@gmail.com>
2022-01-26 07:45:25 +0300

    add a separator between encryption and decryption

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,6 +9,7 @@ public class App {
         System.out.println(" Hello , do you want to hide content meaning from other users ,do you always feel unsafe about your content getting in the wrong hands, worry no more, cipher limited encrypts your message in a way that is only meaningful to you alone "+
                 "Enter your message and choose  encryption, then enter the shift key, to encrypt the message."+"" +
                 "to decrypt copy  the encrypted message and inputed the key that was used to encrypt, you get your message back");
+
         System.out.println(" kindly select one of the following ?" +
                 "Enter one of the following  EncryptText or DecryptMessage ");
 
@@ -25,6 +26,7 @@ public class App {
             Encryption encryption = new Encryption(message,key);
             String cipherText1 = encryption.encrypt();
             System.out.println(cipherText1);
+            System.out.println("*************************************");
         }else if (navigationChoice.equals("DecryptMessage")){
             String StringMessage = new String();
             int ShiftKey = 0;
